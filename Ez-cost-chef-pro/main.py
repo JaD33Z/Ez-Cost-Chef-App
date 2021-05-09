b52276201338671e78da78e13e484b6841d446e3
jonathandavis <jondavis8128@gmail.com>
2021-05-09 17:40:13 -0400

    Add Postgres 2

--- Ez-cost-chef-pro/main.py
+++ Ez-cost-chef-pro/main.py
@@ -15,7 +15,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///chef.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
